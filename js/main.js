b97b000d4e8e33d7f02f36e7cfefffe51b571801
Alfonso Buononato <74600557+AlfonsoBuononato@users.noreply.github.com>
2021-03-24 15:52:50 +0100

    esercizio

---
+++ js/main.js
@@ -0,0 +1,34 @@
+/*Consegna
+1. chiedi all’utente il cognome
+2. inseriscilo in un array con altri cognomi: ‘bianchi’, ‘neri’, ‘rossi’, ‘verdi’, ‘gialli’
+3. stampa la lista ordinata alfabeticamente
+4. scrivi anche la posizione "umana", 1 oppure 2 oppure 3 oppure ecc... 
+(non 0 oppure 1 oppure 2 oppure ecc.. tipici degli array), 
+del nuovo utente inserito sopra all'interno dell'array, 
+dopo l'ordinamento alfabetico*/
+
+//1.CHIEDI IL COGNOME ALL UTENTE.
+
+var cognome = prompt("inserire il cognome.");
+
+console.log(cognome);
+
+//2. INSERIRE IL COGNOME IN UN ARRAY CON ALTRI COGNOMI.
+
+var cognomi = ["bianchi", "neri", "rossi", "verdi", "gialli"];
+
+cognomi.push(cognome);
+
+//3. STAMPA LA LISTA IN ORDINE ALFABETICO.
+
+cognomi.sort();
+
+console.log(cognomi);
+
+//4.SCRIVI ANCHE LA POSIZIONE UMANA DEL NOME INSERITO NELL ARRAY.
+
+var posizione = cognomi.indexOf(cognome);
+
+console.log(posizione + 1);
+
+
